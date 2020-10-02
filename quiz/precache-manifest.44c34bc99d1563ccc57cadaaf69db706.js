cd7626bf3bf86f5f7c418a18ff467d0fcd7fed8b
Sandro444 <tsereteli.sandro0401@gmail.com>
2020-10-02 21:57:14 +0400

    adding build

---
+++ quiz/precache-manifest.44c34bc99d1563ccc57cadaaf69db706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c9886b6f80443bc54cd366b92cdd277",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "b729bdfe4601a3d8330d",
+    "url": "/quiz/static/css/main.50689a57.chunk.css"
+  },
+  {
+    "revision": "3d6129bb7c9eb0bcd981",
+    "url": "/quiz/static/js/2.8e1855bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz/static/js/2.8e1855bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b729bdfe4601a3d8330d",
+    "url": "/quiz/static/js/main.2eb8d0ad.chunk.js"
+  },
+  {
+    "revision": "9008b934ac49466e4064",
+    "url": "/quiz/static/js/runtime-main.573763c4.js"
+  }
+]);
